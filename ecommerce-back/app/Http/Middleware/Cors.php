5c39694dd58c0a975ee13f12a29bb2ea664980f0
montassarbf <montassarbenfraj1234@gmail.com>
2026-03-26 17:44:36 +0100

    fix: update CORS for production

--- ecommerce-back/app/Http/Middleware/Cors.php
+++ ecommerce-back/app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
 
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://localhost:5173')
+        ->header('Access-Control-Allow-Origin', 'https://onclickshop.netlify.app/')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
