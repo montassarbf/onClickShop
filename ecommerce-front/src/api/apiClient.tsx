9dd6952db1aaf05cdb8520c233698d84c245534e
montassarbf <montassarbenfraj1234@gmail.com>
2026-03-26 18:02:35 +0100

    fix: link frontend to backend production

--- ecommerce-front/src/api/apiClient.tsx
+++ ecommerce-front/src/api/apiClient.tsx
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://onclickshop.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
